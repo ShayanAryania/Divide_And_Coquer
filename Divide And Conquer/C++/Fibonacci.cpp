68cbaf46586b91dea2083f117d809dcd18ae8a09
Shayan Aryania <56693370+ShayanAryania@users.noreply.github.com>
2022-03-02 18:52:52 +0330

    Add files via upload

---
+++ Divide And Conquer/C++/Fibonacci.cpp
@@ -0,0 +1,23 @@
+//  Fibonacci Series
+//
+//  Created by Shayan Aryania
+
+
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main ()
+{
+    int n = 9;
+    cout << fib(n);
+    getchar();
+    return 0;
+}
+
